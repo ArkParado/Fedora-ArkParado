80abb7dae7a8dc06d0355bacc3098cfc2c0da711
ArkParado <phuchungvn27@gmail.com>
2026-06-08 07:44:30 +0700

    Add config dotfiles

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+
+return {
+  color_scheme = "Nord (Gogh)",
+  font = wezterm.font("JetBrains Mono"),
+  font_size = 12.0,
+
+  window_background_opacity = 0.85,  -- 0.0 trong suốt hoàn toàn, 1.0 đục hoàn toàn
+}
